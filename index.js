6dc82eb002d49bc3d0cc0174a62156a8a0d2ec5e
Ryan Manchester <ryanm17@gmail.com>
2021-08-18 22:30:03 -0400

    fetches a todo item from json placeholder

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ exports.__esModule = true;
 var axios_1 = require("axios");
 var url = 'https://jsonplaceholder.typicode.com/todos/1';
 axios_1["default"].get(url).then(function (response) {
-    console.log(response.data);
+    var todo = response.data;
+    var id = todo.id;
+    var title = todo.title;
+    var complete = todo.completed;
+    console.log("\n    The Todo with ID: " + id + "\n    Has the title: " + title + "\n    Is it finished? " + complete + "\n    ");
 });
--- index.ts
+++ index.ts
@@ -3,5 +3,14 @@ import axios from 'axios';
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
 axios.get(url).then(response => {
-  console.log(response.data);
+  const todo = response.data;
+  const id = todo.id;
+  const title = todo.title;
+  const complete = todo.completed;
+
+  console.log(`
+    The Todo with ID: ${id}
+    Has the title: ${title}
+    Is it finished? ${complete}
+    `)
 });
